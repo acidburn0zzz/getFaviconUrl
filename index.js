ea378b222d3925cdecac5f469502f3118b8ee447
Olivier de Broqueville <olivier.debroqueville@gmail.com>
2017-10-13 22:44:16 +0200

    Bug fixes.

--- index.js
+++ index.js
@@ -6,40 +6,40 @@ var faviconUrl = "";
 
 console.log('Loading lambda function');
 
-function onError(error){
-    console.log("Error: ${error}")
+function onError(error) {
+  console.log("Error: ${error}")
 }
 
-function getFaviconUrl(url, callback){
-    var urlParts = url.replace('http://','').replace('https://','').split(/[/?#]/);
-    var domain = "https://" + urlParts[0];
-    var options = {
-        url: domain,
-        method: "GET",
-        headers: {
-            "Accept": "text/html"
+function getFaviconUrl(url, callback) {
+  var urlParts = url.replace('http://', '').replace('https://', '').split(/\//);
+  var domain = "https://" + urlParts[0];
+  var options = {
+    url: domain,
+    method: "GET",
+    headers: {
+      "Accept": "text/html"
+    }
+  };
+  request(options, function(error, response, html) {
+    if (!error && response.statusCode == 200) {
+      const $ = cheerio.load(html);
+      console.log("HERE'S THE HTML:" + html);
+      $("link").each(function(i, element) {
+        console.log(element);
+        var rel = $(element).attr("rel");
+        if (rel !== undefined && rel.indexOf("icon") > -1) {
+          faviconUrl = $(element).attr("href");
+          return false;
         }
-    };
-    request(options, function (error, response, html){
-        if (!error && response.statusCode == 200) {
-            const $ = cheerio.load(html);
-            console.log("HERE'S THE HTML:" + html);
-            $("link").each(function(i, element){
-                console.log(element);
-                const rel = $(element).attr("rel");
-                if (rel === "icon" || rel === "shortcut icon" || rel === "fluid-icon") {
-                  faviconUrl = $(element).attr("href");
-                  return false;
-                }
-            });
-            if (faviconUrl.indexOf("/") === 0) { // If path returned is relative
-                faviconUrl = "https://" + domain + faviconUrl;
-            }
-            callback(null, faviconUrl);
-        }
-    });
+      });
+      if (faviconUrl.indexOf("/") === 0) { // If path returned is relative to root folder
+        faviconUrl = domain + faviconUrl;
+      }
+      callback(null, faviconUrl);
+    }
+  });
 }
 
 exports.handler = (event, context, callback) => {
-    getFaviconUrl(event.url, callback);
+  getFaviconUrl(event.url, callback);
 }
